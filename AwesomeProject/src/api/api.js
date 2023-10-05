3d624510763ec9d0c7b853aab70658cd7363d1af
zhuyuhui <zhuyuhui@odianyun.com>
2023-10-05 21:39:35 +0800

    feat: mock请求地址修改

--- AwesomeProject/src/api/api.js
+++ AwesomeProject/src/api/api.js
@@ -5,6 +5,9 @@ export default {
   GET_TRENDING_REPO: (language, since = 'daily') =>
     `https://ghapi.huchen.dev/repositories?language=${language}&since=${since}`,
 
+  // GET_JINGDONG_REPO: url =>
+  //   `https://gitlab.com/ikumock-data/MockDatas/-/raw/JingDong/20220629/functionId/@new/${url}`,
   GET_JINGDONG_REPO: url =>
-    `https://gitlab.com/ikumock-data/MockDatas/-/raw/JingDong/20220629/functionId/@new/${url}`,
+    `https://raw.githubusercontent.com/MiaoPaSiRN/mstartapp/main/AwesomeProject/mock-data/functionId/%40new/${url}`,
 };
+//https://raw.githubusercontent.com/MiaoPaSiRN/mstartapp/main/AwesomeProject/mock-data/functionId/%40new/tab/cart/cart.json
